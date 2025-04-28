11adefbe946131a2f400b4a1416eefda888e8aad
Luqman Dev Diary <luqmansalad00@gmail.com>
2025-04-28 13:11:25 +0300

    added single blog post page and dispay blog post

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,13 +1,72 @@
-import React from 'react'
+import { client } from '@/sanity/lib/client';
+import { groq } from 'next-sanity';
+import { notFound } from 'next/navigation';
+import { PortableTextComponent } from '@/components/PortableTextComponent';
+import { urlFor } from '@/sanity/lib/image';
+import Image from 'next/image';
 
-const page = async ({params}: {params: Promise<{slug: string}>})  => {
-    const {slug} = await params;
-    
-  return (
-    <div className='mt-25'>
-      <h1>Hello World</h1>
-    </div>
-  )
+interface Post {
+  _id: string;
+  title: string;
+  body: any;
+  publishedAt: string;
+  author: {
+    name: string;
+    image: any;
+  };
 }
 
-export default page;
+
+const query = groq`*[_type == "post" && slug.current == $slug][0]{
+  _id,
+  title,
+  body[]{
+    ...,
+    asset->{
+      url
+    }
+  },
+  publishedAt,
+  author->{
+    name,
+    image
+  }
+}`;
+
+export default async function SinglePostPage({ params }: { params: { slug: string } }) {
+  const post: Post | null = await client.fetch(query, { slug: params.slug });
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <div className="mt-25 p-6">
+      <p className='mb-5'>Blog &gt; <span className='text-gray-500'>{post.title}</span></p>
+      <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
+
+      {/* AUTHOR and DATE */}
+      <div className="flex items-center gap-4 text-gray-600 text-sm mb-8">
+        {post.author?.image && (
+          <Image
+            src={urlFor(post.author.image).width(50).height(50).url()}
+            alt={post.author.name}
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+        )}
+        <div>
+          <p className="font-md">By <span className='text-cyan-600'>{post.author?.name}</span></p>
+          <p>Last Updated: {new Date(post.publishedAt).toDateString()}</p>
+        </div>
+      </div>
+
+      <hr className='text-gray-300'/>
+      
+      <div className="prose prose-lg">
+        <PortableTextComponent value={post.body} />
+      </div>
+    </div>
+  );
+}
--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -28,12 +28,11 @@ const BlogList = async () => {
         <div className='text-center py-5 my-2 mx-2 overflow-auto scrollbar-hide'>
             {ArticleCategories.map((category, index) => (
                 <Link href="/" key={index}><span className='border-2 border-gray-300 mr-3 py-1 px-5 rounded-lg  hover:border-cyan-700 transition'>{category}</span></Link>
-
             ))}
         </div>
         <div className='mt-5 mx-5 sm:grid sm:grid-cols-2 gap-2'>
             {posts.map((post:any) => (
-                <div key={post._id} className="bg-white rounded-lg overflow-hidden shadow p-4 flex flex-col justify-between h-full border border-gray-200">
+                <Link href={`/blog/${post.slug.current}`} key={post._id} className="bg-white rounded-lg overflow-hidden shadow p-4 flex flex-col justify-between h-full border border-gray-200">
                 
                 {/* Top Info */}
                 <div className="text-gray-500 text-sm flex gap-4 text-center mb-2">
@@ -42,18 +41,12 @@ const BlogList = async () => {
                 </div>
           
                 {/* Title */}
+                <div>
                 <h2 className="text-lg font-semibold text-gray-800 mb-2">{post.title}</h2>
+                </div>
           
                 {/* Excerpt */}
                 <p className="text-gray-600 text-sm line-clamp-2">{post.excerpt}</p>
-          
-                {/* Read More */}
-                <div className="mt-2">
-                  <Link href={`/blog/${post.slug.current}`} className="text-amber-600 text-sm font-medium hover:underline">
-                    Read More
-                  </Link>
-                </div>
-
                 
           
                 {/* Image */}
@@ -66,7 +59,8 @@ const BlogList = async () => {
                     className="rounded-lg object-cover w-full h-48"
                   />
                 </div>
-              </div>
+              </Link>
+              
               
             ))}
            
