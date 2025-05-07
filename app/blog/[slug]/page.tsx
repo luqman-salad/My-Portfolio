f98f57452131a0ae2b917dcdf9bbaaa388333964
Luqman Dev Diary <luqmansalad00@gmail.com>
2025-05-07 12:11:23 +0300

    chnaged typename for singlepagepost

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -53,13 +53,13 @@ const query = groq`*[_type == "post" && slug.current == $slug][0]{
 
 }`;
 
-type Props = {
+type PageProps = {
   params: {
     slug: string;
   };
 };
 
-const SinglePostPage = async ({params}: Props) => {
+const SinglePostPage = async ({params}: PageProps) => {
   const post: Post | null = await client.fetch(query, { slug: params.slug });
 
   if (!post) {
