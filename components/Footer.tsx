c43f0401ba718c9b7cb448b7bc52099f594ad16b
Luqman Dev Diary <luqmansalad00@gmail.com>
2025-05-08 10:30:36 +0300

    made footer year dynatic

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const Footer = () => {
+
+  const year = new Date().getFullYear();
+
   return (
     <footer className='my-10'>
       <div className='text-center border-t border-t-gray-300 py-5'>
-        <p className='text-gray-600'>&copy; 2025 Luqman Salad</p>
+        <p className='text-gray-600'>&copy; {year} Luqman Salad</p>
     </div>
     </footer>
     
